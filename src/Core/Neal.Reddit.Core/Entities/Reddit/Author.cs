4cdf90b7f464a7457966126bdad068eec6e38e08
TJ Neal <tj@tjneal.com>
2023-09-11 18:10:29 -0500

    Add count to author

--- src/Core/Neal.Reddit.Core/Entities/Reddit/Author.cs
+++ src/Core/Neal.Reddit.Core/Entities/Reddit/Author.cs
@@ -1,7 +1,10 @@
 ﻿namespace Neal.Reddit.Core.Entities.Reddit;
+
 public class Author
 {
     public string Name { get; set; } = string.Empty;
+
+    public int PostCount { get => this.Posts.Count(); }
 
     public IEnumerable<Link> Posts { get; set; } = Enumerable.Empty<Link>();
 }
